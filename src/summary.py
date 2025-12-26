31eb0abc057615d5b0fc449bd5617bf5d647656d
Dante Foulke <Flkdnt@gmail.com>
2025-12-26 15:49:53 -0500

    + summary.py

---
+++ src/summary.py
@@ -0,0 +1,17 @@
+import json
+import logging
+import os
+import re
+from datetime import datetime
+
+from langchain_ollama import ChatOllama
+from langchain_text_splitters import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    # --- LLM Configuration ---
+    ollama_Model = "gemma3:4b"
