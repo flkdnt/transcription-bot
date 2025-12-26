a73a65652352b40c72450a4cfc3ee4467f79a6e0
Dante Foulke <Flkdnt@gmail.com>
2025-12-26 15:47:46 -0500

    logger and adjustments

--- src/download.py
+++ src/download.py
@@ -1,25 +1,11 @@
 import logging
 import os
+from datetime import datetime
 
 import yt_dlp
 
 
-def delete_webm_files(directory):
-    """
-    Deletes all files inside a directory that end with .webm.
-    """
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.endswith(".webm"):
-                file_path = os.path.join(root, file)
-                try:
-                    os.remove(file_path)
-                    print(f"Deleted: {file_path}")
-                except OSError as e:
-                    print(f"Error deleting {file_path}: {e}")
-
-
-def download_file(url, output_dir):
+def download_file(url, output_dir, noplaylist="False", logger=None):
     """
     Downloads a file from a URL using yt-dlp.
 
@@ -39,12 +25,18 @@ def download_file(url, output_dir):
         "extractor_args": {"youtube": {"player_client": ["default"]}},
         "restrictfilenames": "True",
         "format": "bestvideo+bestaudio/best",  # Download the best quality
-        "keepvideo": "True",
+        # "keepvideo": "True",
+        "noplaylist": noplaylist,
+        "writeinfojson": "True",
+        "writesubtitles": "True",
+        "writeautomaticsub": "True",
+        "subtitlesformat": "vtt",
+        # "subtitleslangs": "en",
         "postprocessors": [
-            {  # Convert to m4a(mp4)
-                "key": "FFmpegVideoConvertor",
-                "preferedformat": "m4a",
-            },
+            # {  # Convert to m4a(mp4)
+            #    "key": "FFmpegVideoConvertor",
+            #    "preferedformat": "m4a",
+            # },
             {  # Extract audio using ffmpeg
                 "key": "FFmpegExtractAudio",
                 "preferredcodec": "wav",
@@ -54,10 +46,12 @@ def download_file(url, output_dir):
         ],
     }
 
+    logger.info(f"{datetime.now()}: Starting Download of {url}...")
+
     with yt_dlp.YoutubeDL(params=ydl_opts) as ydl:
         ydl.download(url)
 
-    logger.info(f"File downloaded to: '{output_dir}'")
+    logger.info(f"{datetime.now()}: ...Downloaded to: '{output_dir}'")
 
     return f"{output_dir}"
 
@@ -70,4 +64,3 @@ if __name__ == "__main__":
     video_url = "https://www.youtube.com/watch?v=HhBph2M5ZaU"
     download_directory = "downloads"  # Specify your desired download directory
     download_file(video_url, download_directory)
-    delete_webm_files(download_directory)
