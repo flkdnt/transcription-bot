7c3defe20ce6194c87d2cd246b9b2611df8ace10
Dante Foulke <flkdnt@gmail.com>
2025-12-27 23:03:21 -0500

    major improvements to download
    Signed-off-by: Dante Foulke <flkdnt@gmail.com>

--- src/download.py
+++ src/download.py
@@ -7,33 +7,42 @@ import yt_dlp
 logger = logging.getLogger(__name__)
 
 
-def download_file(url, output_dir, noplaylist="False"):
+def download_audio(url, output_dir, noplaylist="False"):
     """
-    Downloads a file from a URL using yt-dlp.
+    Downloads audio from a YouTube URL using yt-dlp.
+
+    This function utilizes the yt-dlp library to download audio from a
+    given YouTube video URL. It employs a specific postprocessor to extract
+    the audio in WAV format and saves it to the specified output directory.
 
     Args:
-        url: The URL of the file to download.
-        output_dir: The directory where the downloaded file should be saved.
+        url (str): The URL of the YouTube video.
+        output_dir (str): The directory where the downloaded audio file
+            should be saved. The function will create this directory if
+            it doesn't exist.
+        noplaylist (str, optional): A flag indicating whether to handle
+            playlists. Defaults to "False".
+
+    Returns:
+        str: The full path to the downloaded audio file if the download
+             was successful, otherwise None.
     """
 
     # Create the output directory if it doesn't exist
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    ydl_opts = {
+    options = {
         "outtmpl": os.path.join(
             f"{output_dir}/%(title)s", "video.%(ext)s"
         ),  # Customize filename
+        "logger": logger,
         "extractor_args": {"youtube": {"player_client": ["default"]}},
         "restrictfilenames": "True",
-        "format": "bestvideo+bestaudio/best",  # Download the best quality
+        "format": "bestaudio",  # Download the best quality
         # "keepvideo": "True",
         "noplaylist": noplaylist,
         "writeinfojson": "True",
-        "writesubtitles": "True",
-        "writeautomaticsub": "True",
-        "subtitlesformat": "vtt",
-        # "subtitleslangs": "en",
         "postprocessors": [
             # {  # Convert to m4a(mp4)
             #    "key": "FFmpegVideoConvertor",
@@ -48,16 +57,219 @@ def download_file(url, output_dir, noplaylist="False"):
         ],
     }
 
+    download = ytdlp_download(options, url)
+
+    if validate_file(download, "video", ".wav"):
+        logger.info(
+            f"{datetime.now()}: Audio file video.wav has been Downloaded to: {download}"
+        )
+        return download
+    # This is a workaround
+    elif validate_file(download, "video", ".webm"):
+        logger.info(
+            f"{datetime.now()}: Video File video.m4a has been Downloaded to: {download}"
+        )
+        return download
+    else:
+        logger.info(f"{datetime.now()}: Audio file video.wav has NOT been Downloaded!")
+        return None
+
+
+def download_subtitles(url, output_dir, noplaylist="False"):
+    """
+    Downloads subtitles from a YouTube URL using yt-dlp.
+
+    This function utilizes the yt-dlp library to download subtitle files
+    from a given YouTube video URL. It constructs the output filename
+    using a predefined template and handles any potential errors during
+    the download process.
+
+    Args:
+        url (str): The URL of the YouTube video.
+        output_dir (str): The directory where the downloaded subtitle file
+            should be saved. The function will create this directory if it
+            doesn't exist.
+        noplaylist (str, optional):  A flag indicating whether to handle
+            playlists. Defaults to "False".
+
+    Returns:
+        str: The full path to the downloaded subtitle file if the download
+             was successful, otherwise None.
+    """
+
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    options = {
+        "outtmpl": os.path.join(
+            f"{output_dir}/%(title)s", "video.%(ext)s"
+        ),  # Customize filename
+        "logger": logger,
+        "extractor_args": {"youtube": {"player_client": ["default"]}},
+        "restrictfilenames": "True",
+        "noplaylist": noplaylist,
+        "writeinfojson": "True",
+        "writesubtitles": "True",
+        # "writeautomaticsub": "True",
+        "skip_download": "True",
+        "subtitlesformat": "vtt",
+        # "subtitleslangs": "en",
+    }
+
+    download = ytdlp_download(options, url)
+
+    if validate_file(download, "video", ".vtt"):
+        logger.info(
+            f"{datetime.now()}: Subtitle File video*.vtt has been Downloaded to: {download}"
+        )
+        return download
+    else:
+        logger.info(
+            f"{datetime.now()}: Subtitle File video*.vtt has Not been Downloaded!"
+        )
+        return None
+
+
+def download_video(url, output_dir, noplaylist="False"):
+    """
+    Downloads a video from a YouTube URL using yt-dlp.
+
+    This function utilizes the yt-dlp library to download a video from a
+    given YouTube URL. It downloads the best quality video and audio
+    streams and then converts the combined stream to an m4a(mp4) format.
+    It saves the downloaded file to the specified output directory.
+
+    Args:
+        url (str): The URL of the YouTube video.
+        output_dir (str): The directory where the downloaded video file
+            should be saved. The function will create this directory if
+            it doesn't exist.
+        noplaylist (str, optional): A flag indicating whether to handle
+            playlists. Defaults to "False".
+
+    Returns:
+        str: The full path to the downloaded video file if the download
+             was successful, otherwise None.
+    """
+
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    options = {
+        "outtmpl": os.path.join(
+            f"{output_dir}/%(title)s", "video.%(ext)s"
+        ),  # Customize filename
+        "logger": logger,
+        "extractor_args": {"youtube": {"player_client": ["default"]}},
+        "restrictfilenames": "True",
+        "format": "bestvideo+bestaudio/best",  # Download the best quality
+        "noplaylist": noplaylist,
+        "writeinfojson": "True",
+        "print": "after_move:filename",
+        "postprocessors": [
+            {  # Convert to m4a(mp4)
+                "key": "FFmpegVideoConvertor",
+                "preferedformat": "m4a",
+            }
+        ],
+    }
+
+    download = ytdlp_download(options, url)
+
+    if validate_file(download, "video", ".m4a"):
+        logger.info(
+            f"{datetime.now()}: Video File video.m4a has been Downloaded to: {download}"
+        )
+        return download
+    # This is a workaround
+    elif validate_file(download, "video", ".webm"):
+        logger.info(
+            f"{datetime.now()}: Video File video.m4a has been Downloaded to: {download}"
+        )
+        return download
+    else:
+        logger.info(f"{datetime.now()}: Video File video.m4a has Not been Downloaded!")
+        return None
+
+
+def format_path(filename):
+    """
+    Trims the filename from the end of a path and returns the path itself.
+
+    This function removes the filename from the end of a given path string,
+    returning the remaining path. It handles cases where the input filename
+    is empty or if the path consists of only a single part.
+
+    Args:
+        filename (str): The path string from which to remove the filename.
+
+    Returns:
+        str: The path string with the filename removed.
+             Returns None if the input `filename` is empty.
+    """
+    if not filename:
+        logger.warning("No parameter 'filename' passed to format_path")
+        return None
+
+    parts = filename.split(os.sep)
+    if len(parts) > 1:
+        return os.path.join(*parts[:-1])
+    else:
+        return filename
+
+
+def validate_file(path, start_filter, end_filter):
+    """
+    Searches for a file within a given path that starts with 'start_filter' and ends with 'end_filter'
+
+    Args:
+        path: The directory to search within.
+
+    Returns:
+        True if file exists or False if it doesn't exists
+    """
+    try:
+        files = os.listdir(path)
+        for filename in files:
+            logger.info(f"{datetime.now()}: {filename}")
+            if filename.startswith(start_filter) and filename.endswith(end_filter):
+                return True
+            else:
+                return False
+    except FileNotFoundError:
+        return None
+
+
+def ytdlp_download(options, url):
+    """
+    Downloads a file from a YouTube URL using yt-dlp.
+
+    This function utilizes the yt-dlp library to download a file from a
+    given YouTube URL. It handles filename formatting and validation
+    after the download.
+
+    Args:
+        options (dict): A dictionary containing yt-dlp download options.
+        url (str): The URL of the YouTube video.
+        pre_filter (str): A string to prepend to the log message.
+        post_filter (str): A string to append to the log message.
+
+    Returns:
+        str: The full path to the downloaded file if successful,
+             otherwise None.
+    """
     logger.info(f"{datetime.now()}: Starting Download of {url}...")
 
-    with yt_dlp.YoutubeDL(params=ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-        logger.info(info)  # Print the title of the video
-        ydl.download(url)
+    with yt_dlp.YoutubeDL(params=options) as ydl:
+        info = ydl.extract_info(url, download=True)
+        fname = ydl.prepare_filename(info)
+        ydl.close()
 
-    logger.info(f"{datetime.now()}: ...Downloaded to: '{output_dir}'")
+    fname = format_path(fname)
 
-    return f"{output_dir}"
+    return fname
 
 
 if __name__ == "__main__":
@@ -66,4 +278,6 @@ if __name__ == "__main__":
 
     video_url = ""
     download_directory = "downloads"  # Specify your desired download directory
-    download_file(video_url, download_directory)
+    subtitles = download_subtitles(video_url, download_directory)
+    if subtitles is None:
+        download_audio(video_url, download_directory)
