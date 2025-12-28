1270a7df57b7ce7c9b55ba36ef889ea4e3537b56
Dante Foulke <flkdnt@gmail.com>
2025-12-28 18:08:39 -0500

    renamed to utility_llm
    Signed-off-by: Dante Foulke <flkdnt@gmail.com>

--- src/utility_llm.py
+++ src/utility_llm.py
@@ -1,6 +1,5 @@
 import logging
 from datetime import datetime
-from typing import List, Optional
 
 from langchain_ollama import ChatOllama
 from langchain_text_splitters import RecursiveCharacterTextSplitter
